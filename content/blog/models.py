710e872e7fe7a19493ad42e623b62635db43e3c5
rasmus <rasmus@peacegeeks.org>
2018-02-28 14:06:47 -0800

    Fixed problem where the only kind of page I could create was home pages

--- content/blog/models.py
+++ content/blog/models.py
@@ -17,9 +17,6 @@ class HomePage(Page):
 
 
 class BlogIndexPage(Page):
-    parent_page_types = []
-    subpage_types = ['BlogPage']
-
     def get_context(self, request):
         context = super(BlogIndexPage, self).get_context(request)
         context['blog_entries'] = BlogPage.objects.child_of(self).live()
@@ -55,6 +52,3 @@ class BlogPage(Page):
         MultiFieldPanel(Page.promote_panels, 'Common page configuration'),
         ImageChooserPanel('feed_image'),
     ]
-
-    parent_page_types = ['BlogIndexPage']
-    subpage_types = []
