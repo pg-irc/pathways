6f8dc3d6d459b362a82ca8b5cda0c282eab0e968
rasmus <rasmus@peacegeeks.org>
2018-02-28 16:57:11 -0800

    Fixed up urls and generally got things working

--- config/urls.py
+++ config/urls.py
@@ -4,16 +4,22 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
-from wagtail.wagtailadmin import urls as wagtailadmin_urls
-from wagtail.wagtaildocs import urls as wagtaildocs_urls
-from wagtail.wagtailcore import urls as wagtail_urls
+from wagtail.wagtailadmin import urls as blog_admin_urls
+from wagtail.wagtaildocs import urls as blog_documentation_urls
+from wagtail.wagtailcore import urls as blog_urls
+
+from wagtail.api.v2.router import WagtailAPIRouter
+from wagtail.api.v2.endpoints import PagesAPIEndpoint
+from wagtail.wagtailimages.api.v2.endpoints import ImagesAPIEndpoint
+from wagtail.wagtaildocs.api.v2.endpoints import DocumentsAPIEndpoint
+
 from human_services.organizations.viewsets import OrganizationViewSet
 from human_services.locations.viewsets import LocationViewSet, LocationViewSetUnderOrganizations
 from human_services.services.viewsets import ServiceViewSet
 from rest_framework import routers
 from config import documentation
 
-def build_router():
+def build_api_router():
     router = routers.DefaultRouter()
     router.register(r'organizations', OrganizationViewSet, base_name='organization')
     router.register(r'organizations/(?P<organization_id>\w+)/locations',
@@ -28,6 +34,13 @@ def build_router():
 
     return router
 
+def build_cms_router():
+    api_router = WagtailAPIRouter('wagtailapi')
+    api_router.register_endpoint('pages', PagesAPIEndpoint)
+    api_router.register_endpoint('images', ImagesAPIEndpoint)
+    api_router.register_endpoint('documents', DocumentsAPIEndpoint)
+    return api_router
+
 SCHEMA_VIEW = documentation.build_schema_view()
 
 urlpatterns = [
@@ -41,17 +54,19 @@ urlpatterns = [
     url(r'^users/', include('users.urls', namespace='users')),
     url(r'^accounts/', include('allauth.urls')),
 
-    # Your stuff: custom urls includes go here
+    # api docs
     url(r'^swagger(?P<format>.json|.yaml)$', SCHEMA_VIEW.without_ui(cache_timeout=None), name='schema-json'),
     url(r'^swagger/$', SCHEMA_VIEW.with_ui('swagger', cache_timeout=None), name='schema-swagger-ui'),
     url(r'^redoc/$', SCHEMA_VIEW.with_ui('redoc', cache_timeout=None), name='schema-redoc'),
 
-    # wagtail
-    url(r'^cms/', include(wagtailadmin_urls)),
-    url(r'^documents/', include(wagtaildocs_urls)),
-    url(r'^pages/', include(wagtail_urls)), # Ultimately we'll probably want url(r'', include(wagtail_urls)), here
+    # blog
+    url(r'^blog/admin/', include(blog_admin_urls)),
+    url(r'^blog/docs/', include(blog_documentation_urls)),
+    url(r'^blog/', include(blog_urls)),
 
-    url(r'^v1/', include(build_router().urls)),
+    # api
+    url(r'^v1/blog/', build_cms_router().urls),
+    url(r'^v1/', include(build_api_router().urls)),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- content/blog/models.py
+++ content/blog/models.py
@@ -17,6 +17,12 @@ class HomePage(Page):
 
 
 class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
+
     def get_context(self, request):
         context = super(BlogIndexPage, self).get_context(request)
         context['blog_entries'] = BlogPage.objects.child_of(self).live()
