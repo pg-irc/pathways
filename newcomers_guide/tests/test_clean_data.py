3262c40c7c0198bbe64b3d5a6ac1e108361ce4f3
Ian Lin <ian@peacegeeks.org>
2019-06-25 12:12:27 -0700

    Issue #682 Changed clean_up_email_links tests to use clean_text

--- newcomers_guide/tests/test_clean_data.py
+++ newcomers_guide/tests/test_clean_data.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
-from newcomers_guide.clean_data import (clean_text, clean_up_email_links,
-                                         replace_unicode_newlines)
+from newcomers_guide.clean_data import (clean_text, replace_unicode_newlines)
 
 # Need to replace single newlines with space, except when the line before
 # and/or after is a list item or heading. This is because the markdown
@@ -316,20 +315,20 @@ class CleanUpUrlLinksTest(TestCase):
 class CleanUpMailtoLinksTest(TestCase):
     def test_replaces_email_link_with_markdown(self):
         text = 'abc foo@bar.com def'
-        self.assertEqual(clean_up_email_links(text), 'abc Email: [foo@bar.com](mailto:foo@bar.com) def')
+        self.assertEqual(clean_text(text), 'abc Email: [foo@bar.com](mailto:foo@bar.com) def')
 
     def test_excludes_leading_opening_parenthesis(self):
         text = 'abc (foo@bar.com def'
-        self.assertEqual(clean_up_email_links(text), 'abc (Email: [foo@bar.com](mailto:foo@bar.com) def')
+        self.assertEqual(clean_text(text), 'abc (Email: [foo@bar.com](mailto:foo@bar.com) def')
 
     def test_excludes_trailing_dot_from_link(self):
         text = 'abc foo@bar.com. Def'
-        self.assertEqual(clean_up_email_links(text), 'abc Email: [foo@bar.com](mailto:foo@bar.com). Def')
+        self.assertEqual(clean_text(text), 'abc Email: [foo@bar.com](mailto:foo@bar.com). Def')
 
     def test_excludes_trailing_comma_from_link(self):
         text = 'abc foo@bar.com, def'
-        self.assertEqual(clean_up_email_links(text), 'abc Email: [foo@bar.com](mailto:foo@bar.com), def')
+        self.assertEqual(clean_text(text), 'abc Email: [foo@bar.com](mailto:foo@bar.com), def')
 
     def test_excludes_trailing_closing_parenthesis(self):
         text = 'abc foo@bar.com) def'
-        self.assertEqual(clean_up_email_links(text), 'abc Email: [foo@bar.com](mailto:foo@bar.com)) def')
+        self.assertEqual(clean_text(text), 'abc Email: [foo@bar.com](mailto:foo@bar.com)) def')
